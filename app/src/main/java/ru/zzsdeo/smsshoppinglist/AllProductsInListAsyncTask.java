e27e04c7ec28478ef4594e95f33712074f90fa3d
zzsdeo <zzsdeo@gmail.com>
2014-11-26 16:38:30 +0300

    Initial Commit

---
+++ app/src/main/java/ru/zzsdeo/smsshoppinglist/AllProductsInListAsyncTask.java
@@ -0,0 +1,60 @@
+package ru.zzsdeo.smsshoppinglist;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+public class AllProductsInListAsyncTask extends AsyncTask<Void, Integer, Void> {
+
+    private ProgressDialog progressBar;
+    private Context context;
+    private int i;
+
+    public AllProductsInListAsyncTask (Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        progressBar = new ProgressDialog(context);
+        progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressBar.setIndeterminate(true);
+        progressBar.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... option) {
+        progressBar.setMessage(context.getString(R.string.added) + option[0]);
+    }
+
+    @Override
+    protected void onPostExecute(Void unused) {
+        progressBar.dismiss();
+        int sum = i - 1;
+        Toast.makeText(context, context.getString(R.string.added) + sum, Toast.LENGTH_LONG).show();
+        ((Activity)context).finish();
+    }
+
+    @Override
+    protected Void doInBackground(Void... unused) {
+        context.getContentResolver().delete(ShoppingListContentProvider.CONTENT_URI_LIST, null, null);
+        Cursor c = context.getContentResolver().query(ShoppingListContentProvider.CONTENT_URI_PRODUCTS, null, null, null, null);
+        i = 1;
+        if (c.moveToFirst()) {
+            ContentValues values = new ContentValues();
+            do {
+                values.put(ListTable.COLUMN_ITEM, c.getString(c.getColumnIndex(ProductsTable.COLUMN_ITEM)));
+                values.put(ListTable.COLUMN_CHECKED, 0);
+                context.getContentResolver().insert(ShoppingListContentProvider.CONTENT_URI_LIST, values);
+                values.clear();
+                publishProgress(i);
+                i++;
+            } while (c.moveToNext());
+        }
+        return  null;
+    }
+}
--- app/src/main/java/ru/zzsdeo/smsshoppinglist/SettingsFragment.java
+++ app/src/main/java/ru/zzsdeo/smsshoppinglist/SettingsFragment.java
@@ -1,11 +1,9 @@
 package ru.zzsdeo.smsshoppinglist;
 
 import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
@@ -32,18 +30,7 @@ public class SettingsFragment extends PreferenceFragment {
         allProductsInList.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                getActivity().getContentResolver().delete(ShoppingListContentProvider.CONTENT_URI_LIST, null, null);
-                Cursor c = getActivity().getContentResolver().query(ShoppingListContentProvider.CONTENT_URI_PRODUCTS, null, null, null, null);
-                if (c.moveToFirst()) {
-                    ContentValues values = new ContentValues();
-                    do {
-                        values.put(ListTable.COLUMN_ITEM, c.getString(c.getColumnIndex(ProductsTable.COLUMN_ITEM)));
-                        values.put(ListTable.COLUMN_CHECKED, 0);
-                        getActivity().getContentResolver().insert(ShoppingListContentProvider.CONTENT_URI_LIST, values);
-                        values.clear();
-                    } while (c.moveToNext());
-                }
-                getActivity().finish();
+                new AllProductsInListAsyncTask(getActivity()).execute();
                 return true;
             }
         });
